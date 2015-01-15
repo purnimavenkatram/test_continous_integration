0059cc7ed0da201d67e7f28306337c5ef1148c4c
root <root@rwc1vm05.(none)>
2015-01-14 23:47:30 -0800

    Change to test ci

--- webserver.js
+++ webserver.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
   response.write("<title>This is a test web app</title>");
   response.write("</head>");
   response.write("<body>");
-  response.write("Don't think this will work!");
+  response.write("Baby step towards Kickstart continous integration!");
   response.write("</body>");
   response.write("</html>");
   response.end();
